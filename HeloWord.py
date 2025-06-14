c5b66f09a311d88b01abd0cbbaacd70f310acf22
wibasi <williamsylva@gmail.com>
2025-06-14 13:48:21 -0300

    Primeiro commit

---
+++ HeloWord.py
@@ -0,0 +1,1 @@
+print ("!Olá Brasao187 Word!")
---
+++ aula1.py
@@ -0,0 +1,5 @@
+numero1 = int (input ("digite um número inteiro: "))
+numero2 = int (input ("digite um número inteiro: "))
+soma = numero1 + numero2
+
+print("A soma dos dois números inteiros é: : ",soma)
